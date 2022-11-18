a0dae7a28ed0b6687a3da85fbf1458d4975192be
oiroe <pathiti.b@gmail.com>
2022-11-18 14:16:44 +0700

    update.h_file

--- libftprintf.h
+++ libftprintf.h
@@ -9,3 +9,17 @@
 /*   Updated: 2022/11/13 20:43:56 by pboonpro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+#define LIBFTPRINTF_H
+
+#include <unistd.h>
+
+int	c(int a)
+int	s(char *str);
+int	u(unsigned int num);
+int	upperx(int n);
+int	x(int n);
+int	d(int num);
+
+#endif
